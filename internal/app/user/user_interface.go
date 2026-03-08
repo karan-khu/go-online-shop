1cf355610419d2b35d344701929dd80ef0e97d07
อาร์ม-Karan <karan.khu@dplusonline.net>
2026-03-08 18:24:20 +0700

    Remove unused user-related files and middleware for rate limiting and request timeout; streamline user interface by retaining only the UserRepository interface.

--- internal/app/user/user_interface.go
+++ internal/app/user/user_interface.go
@@ -1,9 +1,5 @@
 package user
 
-type UserHttpHandler interface{}
-
-type UserUseCase interface{}
-
 type UserRepository interface {
 	Creating(user *UserEntity) (*UserEntity, error)
 	FindById(userId string) (*UserEntity, error)
