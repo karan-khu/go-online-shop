188cbb113c67d01b31b71e20631b61f1afa3899e
อาร์ม-Karan <karan.khu@dplusonline.net>
2026-03-16 17:40:44 +0700

    Refactor PurchaseHistoryEntity and update purchase repository methods to utilize jinzhu/copier for object mapping, enhancing code clarity and reducing redundancy in purchase listing and creation processes.

--- internal/app/purchase/purchase_entity.go
+++ internal/app/purchase/purchase_entity.go
@@ -1,20 +1,18 @@
 package purchase
 
-import "time"
+import (
+	"time"
+)
 
 type PurchaseHistoryEntity struct {
-	PurchaseId      int       `gorm:"column:PurchaseId;primaryKey;autoIncrement"`
-	BuyerId         string    `gorm:"column:BuyerId"`
-	ItemId          int       `gorm:"column:ItemId"`
-	ItemName        string    `gorm:"column:ItemName;size:255"`
-	ItemDescription string    `gorm:"column:ItemDescription;size:max"`
-	ItemPrice       int       `gorm:"column:ItemPrice"`
-	Quantity        int       `gorm:"column:Quantity"`
-	Type            string    `gorm:"column:Type;size:10;comment:BUY/SELL"`
-	ActiveStatus    string    `gorm:"column:ActiveStatus;size:20;default:AVAILABLE"`
-	CreatedAt       time.Time `gorm:"column:CreatedAt;autoCreateTime"`
-}
-
-func (PurchaseHistoryEntity) TableName() string {
-	return "GOST_PurchaseHistories"
+	PurchaseId      int
+	BuyerId         string
+	ItemId          int
+	ItemName        string
+	ItemDescription string
+	ItemPrice       int
+	Quantity        int
+	Type            string
+	ActiveStatus    string
+	CreatedAt       time.Time
 }
--- internal/app/purchase/purchase_repository.go
+++ internal/app/purchase/purchase_repository.go
@@ -4,10 +4,12 @@ import (
 	"errors"
 	"log/slog"
 
+	"github.com/jinzhu/copier"
 	"gorm.io/gorm"
 
 	"github.com/karan-khu/go-online-shop/config"
 	"github.com/karan-khu/go-online-shop/pkg/database"
+	"github.com/karan-khu/go-online-shop/pkg/database/models"
 )
 
 type purchaseRepositoryImpl struct {
@@ -22,26 +24,32 @@ func NewPurchaseRepository(logger *slog.Logger, conf *config.Config) PurchaseRep
 	}
 }
 
-func (r *purchaseRepositoryImpl) Listing(userId string) ([]*PurchaseHistoryEntity, error) {
-	listPurchase := make([]*PurchaseHistoryEntity, 0)
-	if err := r.db.Connect().Where("BuyerId = ? AND ActiveStatus = ?", userId, "AVAILABLE").Find(&listPurchase).Error; err != nil {
+func (r *purchaseRepositoryImpl) Listing(userId string) (results []*PurchaseHistoryEntity, err error) {
+	purchaseRecords := make([]*models.PurchaseHistoryRecord, 0)
+	if err := r.db.Connect().Where("BuyerId = ? AND ActiveStatus = ?", userId, "AVAILABLE").Find(&purchaseRecords).Error; err != nil {
 		r.logger.Error("failed to list purchase", "error", err)
 		return nil, errors.New("failed to list purchase")
 	}
 
-	return listPurchase, nil
+	copier.Copy(results, purchaseRecords)
+	return results, nil
 }
 
-func (r *purchaseRepositoryImpl) Create(tx *gorm.DB, purchase *PurchaseHistoryEntity) (*PurchaseHistoryEntity, error) {
+func (r *purchaseRepositoryImpl) Create(tx *gorm.DB, purchase *PurchaseHistoryEntity) (result *PurchaseHistoryEntity, err error) {
 	conn := r.db.Connect()
 	if tx != nil {
 		conn = tx
 	}
 
-	newPurchase := new(PurchaseHistoryEntity)
-	if err := conn.Create(purchase).Scan(newPurchase).Error; err != nil {
+	newPurchase := new(models.PurchaseHistoryRecord)
+	copier.Copy(newPurchase, purchase)
+
+	purchaseRecord := new(models.PurchaseHistoryRecord)
+	if err := conn.Create(newPurchase).Scan(purchaseRecord).Error; err != nil {
 		r.logger.Error("failed to create purchase", "error", err)
 		return nil, errors.New("failed to create purchase")
 	}
-	return newPurchase, nil
+
+	copier.Copy(result, purchaseRecord)
+	return result, nil
 }
